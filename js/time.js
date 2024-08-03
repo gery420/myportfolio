40c12d1b9561bc4858f44faae01e514ebc85badb
Ashish <mashishkr025@gmail.com>
2024-08-04 00:33:30 +0530

    Re-Design

--- js/time.js
+++ js/time.js
@@ -21,24 +21,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     startTime();
 
-    function greetings() {
-        var t = new Date();
-        var hours = t.getHours();
-
-        if (((hours>=21) && (hours<24)) || (hours>=0) && (hours<4)){
-            document.getElementById('greet').innerHTML = 'Good Night!';
-        } else if ((hours>=4)&&(hours<12)){
-            document.getElementById('greet').innerHTML = 'Good Morning';
-        } else if ((hours>=12)&&(hours<16)){
-            document.getElementById('greet').innerHTML = 'Good Afternoon';
-        }else if ((hours>=16)&&(hours<20)){
-            document.getElementById('greet').innerHTML = 'Good Evening';
-        }else if ((hours>=20)&&(hours<21)){
-            document.getElementById('greet').innerHTML = 'Hope you had a <br>Good Day!';
-        } else {
-            document.getElementById('greet').innerHTML = 'Welcome';
-        }
-    }
-    greetings();
-
 });
