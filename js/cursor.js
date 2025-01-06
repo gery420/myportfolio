cd2115caa5d26e63c57af0c234e798deb0516d03
Ashish <mashishkr025@gmail.com>
2025-01-06 23:58:17 +0530

    fixes

--- js/cursor.js
+++ js/cursor.js
@@ -28,40 +28,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
     updateCursor();
 
-
-    const smallHover = document.querySelectorAll('#time, .ash');
-    smallHover.forEach(element => {
-        element.addEventListener('mouseenter', () => {
-            customCursor.classList.add('shover');
-        });
-        element.addEventListener('mouseleave', () => {
-            customCursor.classList.remove('shover');
-        });
-    });
-    const bigHover = document.querySelectorAll('a');
-    bigHover.forEach(element => {
-        element.addEventListener('mouseenter', () => {
-            customCursor.classList.add('bhover');
-        });
-        element.addEventListener('mouseleave', () => {
-            customCursor.classList.remove('bhover');
-        });
-    });
-
-    //CURSOR-hide
-
-    //const hideCursorElements = document.querySelectorAll('#time');
-    //hideCursorElements.forEach(element => {
-    //    element.addEventListener('mouseenter', () => {
-    //        customCursor.style.opacity = '0';
-    //        secondaryCursor.style.opacity = '0';
-    //    });
-    //    element.addEventListener('mouseleave', () => {
-    //        customCursor.style.opacity = '1';
-    //        secondaryCursor.style.opacity = '1';
-    //    });
-    //});
-
-
 });
 
